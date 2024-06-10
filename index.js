6352e45060c58f5d5dcf6a20ef72325710716636
WatashiYawa <janielapollo12@gmail.com>
2024-06-10 10:56:27 +0000

    Fix play function to handle inactivity correctly

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
-const { Client, GatewayIntentBits } = require('discord.js');
-const { playYouTube } = require('./commands/playYouTube');
-const { playSpotify } = require('./commands/playSpotify');
-const { play, skip, stop, pause, resume, status, listSongs } = require('./commands/playerControl');
-const { help } = require('./commands/help');
-require('dotenv').config();
+const { Client, GatewayIntentBits } = require("discord.js");
+const { playYouTube } = require("./commands/playYouTube");
+const { playSpotify } = require("./commands/playSpotify");
+const {
+    play,
+    skip,
+    stop,
+    pause,
+    resume,
+    status,
+    listSongs,
+} = require("./commands/playerControl");
+const { help } = require("./commands/help");
+require("dotenv").config();
 
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+        GatewayIntentBits.MessageContent,
+    ],
 });
 
 const queue = new Map();
 
-client.once('ready', () => {
+client.once("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', async message => {
-    const args = message.content.split(' ');
+client.on("messageCreate", async (message) => {
+    const args = message.content.split(" ");
     const command = args.shift().toLowerCase();
 
     const serverQueue = queue.get(message.guild.id);
 
-    if (command === '!playyt') {
+    if (command === "!playyt") {
         await playYouTube(message, serverQueue, args, queue);
-    } else if (command === '!playsp') {
+    } else if (command === "!playsp") {
         await playSpotify(message, serverQueue, args, queue);
-    } else if (command === '!skip') {
+    } else if (command === "!skip") {
         skip(message, serverQueue);
-    } else if (command === '!stop') {
+    } else if (command === "!stop") {
         stop(message, serverQueue);
-    } else if (command === '!pause') {
+    } else if (command === "!pause") {
         pause(message, serverQueue);
-    } else if (command === '!resume') {
+    } else if (command === "!resume") {
         resume(message, serverQueue);
-    } else if (command === '!status') {
+    } else if (command === "!status") {
         status(message);
-    } else if (command === '!list') {
+    } else if (command === "!list") {
         listSongs(message, serverQueue);
-    } else if (command === '!help') {
+    } else if (command === "!help") {
         help(message);
     }
 });
