48350165da6a49d5f627ae82df7888992fe81d41
WatashiYawa <janielapollo12@gmail.com>
2024-06-10 11:31:48 +0000

    Add improved error handling and URL type distinction

--- commands/playSpotify.js
+++ commands/playSpotify.js
@@ -1,39 +1,52 @@
-const { joinVoiceChannel, createAudioPlayer, createAudioResource, AudioPlayerStatus, VoiceConnectionStatus } = require('@discordjs/voice');
-const { PermissionsBitField } = require('discord.js');
-const { play } = require('./playerControl');
-const SpotifyWebApi = require('spotify-web-api-node');
-require('dotenv').config();
+const {
+    joinVoiceChannel,
+    createAudioPlayer,
+    createAudioResource,
+    AudioPlayerStatus,
+    VoiceConnectionStatus,
+} = require("@discordjs/voice");
+const { PermissionsBitField } = require("discord.js");
+const { play } = require("./playerControl");
+const SpotifyWebApi = require("spotify-web-api-node");
+require("dotenv").config();
 
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-    redirectUri: process.env.SPOTIFY_REDIRECT_URI
+    redirectUri: process.env.SPOTIFY_REDIRECT_URI,
 });
 
 // Retrieve an access token
 spotifyApi.clientCredentialsGrant().then(
-    data => {
-        spotifyApi.setAccessToken(data.body['access_token']);
+    (data) => {
+        spotifyApi.setAccessToken(data.body["access_token"]);
     },
-    err => {
-        console.error('Error retrieving access token from Spotify', err);
-    }
+    (err) => {
+        console.error("Error retrieving access token from Spotify", err);
+    },
 );
 
 async function playSpotify(message, serverQueue, args, queue) {
-    const query = args.join(' ');
+    const query = args.join(" ");
     if (!query) {
-        return message.channel.send('Please provide a search query.');
+        return message.channel.send("Please provide a search query.");
     }
 
     const voiceChannel = message.member.voice.channel;
     if (!voiceChannel) {
-        return message.channel.send('You need to be in a voice channel to play music!');
+        return message.channel.send(
+            "You need to be in a voice channel to play music!",
+        );
     }
 
     const permissions = voiceChannel.permissionsFor(message.client.user);
-    if (!permissions.has(PermissionsBitField.Flags.Connect) || !permissions.has(PermissionsBitField.Flags.Speak)) {
-        return message.channel.send('I need the permissions to join and speak in your voice channel!');
+    if (
+        !permissions.has(PermissionsBitField.Flags.Connect) ||
+        !permissions.has(PermissionsBitField.Flags.Speak)
+    ) {
+        return message.channel.send(
+            "I need the permissions to join and speak in your voice channel!",
+        );
     }
 
     try {
@@ -41,7 +54,7 @@ async function playSpotify(message, serverQueue, args, queue) {
         const tracks = data.body.tracks.items;
 
         if (!tracks.length) {
-            return message.channel.send('No results found.');
+            return message.channel.send("No results found.");
         }
 
         const song = {
@@ -57,7 +70,7 @@ async function playSpotify(message, serverQueue, args, queue) {
                 player: createAudioPlayer(),
                 songs: [],
                 playing: true,
-                timeout: null
+                timeout: null,
             };
 
             queue.set(message.guild.id, queueContruct);
@@ -75,9 +88,14 @@ async function playSpotify(message, serverQueue, args, queue) {
 
                 play(message.guild, queueContruct.songs[0], queue);
             } catch (err) {
-                console.error('Error connecting to voice channel:', err.message);
+                console.error(
+                    "Error connecting to voice channel:",
+                    err.message,
+                );
                 queue.delete(message.guild.id);
-                return message.channel.send('There was an error connecting to the voice channel.');
+                return message.channel.send(
+                    "There was an error connecting to the voice channel.",
+                );
             }
         } else {
             clearTimeout(serverQueue.timeout);
@@ -85,10 +103,12 @@ async function playSpotify(message, serverQueue, args, queue) {
             if (serverQueue.player.state.status === AudioPlayerStatus.Idle) {
                 play(message.guild, serverQueue.songs[0], queue);
             }
-            return message.channel.send(`**${song.title}** has been added to the queue!`);
+            return message.channel.send(
+                `**${song.title}** has been added to the queue!`,
+            );
         }
     } catch (error) {
-        console.error('Error executing Spotify command:', error.message);
+        console.error("Error executing Spotify command:", error.message);
         message.channel.send(`An error occurred: ${error.message}`);
     }
 }
